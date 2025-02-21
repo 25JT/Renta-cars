fe027a6883c6c5088e9519134d09b68a49d73704
25JT <jhonierlucumimeza@gmial.com>
2025-02-21 15:35:45 -0500

    Desarrollo fase 1  nav completa, y segundo indice

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#N_logo', {
+  strings: ["Nombre de Empresa", "Bienvenidos"],
+  typeSpeed: 40,
+  backSpeed: 30,
+  loop: true,
+  showCursor: false
+});
+
+document.getElementById('horario').innerHTML = 'LUNES A VIERNES <br/>8:00 AM - 5:00 PM';
+
+AOS.init();
+
+
