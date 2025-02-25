c41428e2c7714354a6fbc3b3ccd76431f8954aea
25JT <jhonierlucumimeza@gmial.com>
2025-02-25 10:39:59 -0500

    Rquisitos alquilar y formas de pago

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ document.getElementById('horario').innerHTML = 'LUNES A VIERNES </br>8:00 AM - 5
 document.getElementById('Presentacion').innerHTML = 'En TRACKER RENTA CAR somos una empresa especializa en ofrecer servicios personalizados y de alta calidad en el sector de transporte y alquiler de vehículos con una amplia gama y variedad. </br> Es muy importante para nosotros Garantizar la satisfacción total de nuestros clientes, cubriendo integralmente sus necesidades, expectativas y requerimientos cuando alquila con nosotros un vehículo.'
 document.getElementById('alquiler-ti').innerHTML = 'alquiler de autos en el aeropuerto Alfonso Bonilla Aragón';
 document.getElementById('Cautos').innerHTML = 'NUESTROS AUTOS'
+document.getElementById('Ralquilar').innerHTML = 'Requisitos para alquilar un vehículo en el aeropuerto Alfonso Bonilla Aragón '
+document.getElementById('Fpago').innerHTML = 'Formas de pago'
 
 function Masinformacion(nombre, marca, modelo, motor, transmision, direccion,combustible,comodidades,capacidad,puertas ) {
   
