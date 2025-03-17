7781b0056b70de1ef3b868bee2044e58e6f79c97
25JT <jhonierlucumimeza@gmial.com>
2025-03-17 17:43:48 -0500

    Ver 1.0
    Finalizacion de la pagina

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ let numero = "3014414701";
 AOS.init();
 var typed = new Typed('#N_logo', {
   strings: ["TRACKER RENTA CAR", "Bienvenidos"],
-  typeSpeed: 40,
+  typeSpeed: 50,
   backSpeed: 30,
   loop: true,
   showCursor: false
 });
 
 const contenido = {
-    horario: "LUNES A VIERNES </br>8:00 AM - 5:00 PM",
+    horario: "DISPONIBILIDAD </br>24/7",
     Presentacion: "En TRACKER RENTA CAR somos una empresa especializada en ofrecer servicios personalizados y de alta calidad en el sector de transporte y alquiler de vehículos con una amplia gama y variedad. </br> Es muy importante para nosotros garantizar la satisfacción total de nuestros clientes, cubriendo integralmente sus necesidades, expectativas y requerimientos cuando alquila con nosotros un vehículo.",
     "alquiler-ti": "Alquiler de autos en el aeropuerto Alfonso Bonilla Aragón",
     Cautos: "NUESTROS AUTOS",
