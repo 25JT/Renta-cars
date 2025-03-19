3a8b797fcef2f453b0b73ac654935e8172fb9db2
25JT <jhonierlucumimeza@gmail.com>
2025-03-19 08:15:48 -0500

    cambio en el nombre

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var typed = new Typed('#N_logo', {
 
 const contenido = {
     horario: "DISPONIBILIDAD </br>24/7",
-    Presentacion: "En TRACKER RENTA CAR somos una empresa especializada en ofrecer servicios personalizados y de alta calidad en el sector de transporte y alquiler de vehículos con una amplia gama y variedad. </br> Es muy importante para nosotros garantizar la satisfacción total de nuestros clientes, cubriendo integralmente sus necesidades, expectativas y requerimientos cuando alquila con nosotros un vehículo.",
+    Presentacion: "En TRAVELING RENTA CAR somos una empresa especializada en ofrecer servicios personalizados y de alta calidad en el sector de transporte y alquiler de vehículos con una amplia gama y variedad. </br> Es muy importante para nosotros garantizar la satisfacción total de nuestros clientes, cubriendo integralmente sus necesidades, expectativas y requerimientos cuando alquila con nosotros un vehículo.",
     "alquiler-ti": "Alquiler de autos en el aeropuerto Alfonso Bonilla Aragón",
     Cautos: "NUESTROS AUTOS",
     Ralquilar: "Requisitos para alquilar un vehículo en el aeropuerto Alfonso Bonilla Aragón",
